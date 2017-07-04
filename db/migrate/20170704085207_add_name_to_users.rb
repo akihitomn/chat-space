3f618420da0f3803c2ce8398c60a84bb8b3acdf6
DESKTOP-56N9A7J\akihito_mano <khtmn8@gmail.com>
2017-07-04 18:43:51 +0900

    Add_null_false_to_user_column

--- db/migrate/20170704085207_add_name_to_users.rb
+++ db/migrate/20170704085207_add_name_to_users.rb
@@ -1,5 +1,6 @@
 class AddNameToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :name, :text
+    add_column :users, :name, :text, null: false
+    add_index :users, :name, unique: true
   end
 end
