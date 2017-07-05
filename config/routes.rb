c8b74902cd5431fca38ae86b09bd84f3d3be50e1
DESKTOP-56N9A7J\akihito_mano <khtmn8@gmail.com>
2017-07-05 14:57:30 +0900

    edit_routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:edit, :update]
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'messages#index'
+  resources :users, only: [:edit, :update]
+  
 end
