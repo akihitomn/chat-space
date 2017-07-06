a2bfef29525f2606b3bf357469fc56362f8de151
DESKTOP-56N9A7J\akihito_mano <khtmn8@gmail.com>
2017-07-06 15:22:48 +0900

    remove_index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170706020942) do
+ActiveRecord::Schema.define(version: 20170706061429) do
 
   create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "user_id"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20170706020942) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_groups_on_name"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
