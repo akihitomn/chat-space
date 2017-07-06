de940d6f3ddaab8964faa0293a225e236ed047c9
DESKTOP-56N9A7J\akihito_mano <khtmn8@gmail.com>
2017-07-06 14:04:06 +0900

    set_routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   resources :users, only: [:edit, :update]
-  
+  resources :groups, only: [:new]
+  post 'groups/:group_id/messages' => 'groups#create'
 end
