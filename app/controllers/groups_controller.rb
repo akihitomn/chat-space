5ba7156332924ec9c79d8b1f132ac36c84842d5a
DESKTOP-56N9A7J\akihito_mano <khtmn8@gmail.com>
2017-07-08 16:24:54 +0900

    グループ作成機能実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,31 @@
 class GroupsController < ApplicationController
 
   def new
+    @group = Group.new
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to "/", notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
   def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    group = Group.find(params[:id])
+    group.update(group_params)
+    redirect_to "/"
+  end
+
+private
+  def group_params
+    params.require(:group).permit(:name, :user_ids)
   end
 
 end
