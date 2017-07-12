9d2dd7e2ef92c9b8bcbe10c092585b7828a570c8
DESKTOP-56N9A7J\akihito_mano <khtmn8@gmail.com>
2017-07-12 14:11:49 +0900

    setting_route_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,17 @@
 class MessagesController < ApplicationController
 
-def create
-end
+  def index
+    @message = Message.new
+    @group = Group.find(params[:group_id])
+  end
+
+  def create
+    Message.create(message_params)
+  end
+
+private
+  def message_params
+    params.require(:message).permit(:message, user_id: current_user.id, group_id: params[:group_id])
+  end
 
 end
