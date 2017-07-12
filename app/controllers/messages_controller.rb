11167e9a5ae5fcfba62fa55635ff46aa6fc94f1f
DESKTOP-56N9A7J\akihito_mano <khtmn8@gmail.com>
2017-07-12 15:31:37 +0900

    edit_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,26 @@
 class MessagesController < ApplicationController
+  before_action :set_action
 
   def index
     @message = Message.new
-    @group = Group.find(params[:group_id])
   end
 
   def create
-    Message.create(message_params)
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@group.id)
+    else
+      render :index, alert: 'メッセージが保存できませんでした。'
+    end
   end
 
 private
   def message_params
     params.require(:message).permit(:message, user_id: current_user.id, group_id: params[:group_id])
+  end
+
+  def set_action
+    @group = Group.find(params[:group_id])
   end
 
 end
