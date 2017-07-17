d5ea69d1f3921f114203460edde3464f464cc679
DESKTOP-56N9A7J\akihito_mano <khtmn8@gmail.com>
2017-07-17 20:28:30 +0900

    不要なid削除

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,8 +2,6 @@ FactoryGirl.define do
 
   factory :message do
     message     "test"
-    group_id    "1"
-    user_id     "1"
     image { Rack::Test::UploadedFile.new Rails.root.join('spec/fixtures/sample.jpeg'), 'sample/jpeg'}
     created_at       { Faker::Time.between(5.days.ago, 3.days.ago, :all) }
     updated_at       { Faker::Time.between(2.days.ago, 1.days.ago, :all) }
